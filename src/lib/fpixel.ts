e1a9825dc6e32c0d1e98e46c5c65c385abd6f594
infisparks <infisparks@gmail.com>
2026-08-17 12:57:42 +0530

    sdf

--- src/lib/fpixel.ts
+++ src/lib/fpixel.ts
@@ -1,5 +1,5 @@
 export const FB_PIXEL_ID =
-  process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID || "698546976470012";
+  process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID || "1264326608572071";
 
 declare global {
   interface Window {
